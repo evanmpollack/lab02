9c8ebaf03bfe4be6e0111808cee0d92c0e7d300c
jkat718 <jkat718@users.noreply.github.com>
2020-07-01 21:50:06 -0400

    add Jason's lab5
    Added my lab5 files
    +Added nav includes on jstart.php
    
    Also renamed "epstart.php" to "estart.php" (link was broken otherwise)

---
+++ estart.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Lab 5 - Metallica</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<header>
+			<h1>Metallica</h1>
+			<div>A band that played music.</div>
+			<?php include "inc/nav.php"; ?>
+		</header>
+		<article>
+			<h2>About Metallica</h2>
+			<figure>
+				<img src="images/logo.png" alt="Logo">
+				<figcaption>Band logo</figcaption>
+			</figure>
+			<p>Metallica is an American heavy metal band. The band was formed in 1981 in Los
+				Angeles by vocalist/guitarist James Hetfield and drummer Lars Ulrich, and has been
+				based in San Francisco for most of its career. The band's fast tempos,
+				instrumentals and aggressive musicianship made them one of the founding "big four"
+				bands of thrash metal, alongside Megadeth, Anthrax and Slayer. Metallica's current
+				lineup comprises founding members and primary songwriters Hetfield and Ulrich,
+				longtime lead guitarist Kirk Hammett and bassist Robert Trujillo. Guitarist Dave
+				Mustaine (who went on to form Megadeth after being fired from the band) and
+				bassists Ron McGovney, Cliff Burton (who died in a bus accident in Sweden in 1986)
+			and Jason Newsted are former members of the band.</p>
+			<p>Metallica earned a growing fan base in the underground music community and won
+				critical acclaim with its first five albums. The band's third album, Master of
+				Puppets (1986), was described as one of the heaviest and most influential thrash
+				metal albums. Its eponymous fifth album, Metallica (1991), the band's first to
+				root predominantly in heavy metal, appealed to a more mainstream audience,
+				achieving substantial commercial success and selling over 16 million copies in the
+				United States to date, making it the best-selling album of the SoundScan era.
+				After experimenting with different genres and directions in subsequent releases,
+				the band returned to its thrash metal roots with the release of its ninth album,
+				Death Magnetic (2008), which drew similar praise to that of the band's earlier
+			albums.</p>
+			<p>In 2000, Metallica led the case against the peer-to-peer file sharing service
+				Napster, in which the band and several other artists filed lawsuits against the
+				service for sharing their copyright-protected material without consent; after
+				reaching a settlement, Napster became a pay-to-use service in 2003. Metallica was
+				the subject of the acclaimed 2004 documentary film Metallica: Some Kind of Monster,
+				which documented the troubled production of the band's eighth album,
+				St. Anger (2003), and the internal struggles within the band at the time. In 2009,
+				Metallica was inducted into the Rock and Roll Hall of Fame. The band wrote the
+				screenplay for and starred in the 2013 IMAX concert film Metallica: Through the
+			Never, in which the band performed live against a fictional thriller storyline.</p>
+			<h2>Style and lyrical themes</h2>
+			<p>Metallica was influenced by early heavy metal and hard rock bands and artists Black
+				Sabbath, Deep Purple, Kiss, Led Zeppelin, Queen, Ted Nugent, AC/DC, Rush,
+				Aerosmith, Judas Priest, Scorpions and by new wave of British heavy metal (NWOBHM)
+				bands Venom, Motörhead, Saxon, Diamond Head, Blitzkrieg, and Iron Maiden. Early
+				punk rock bands Ramones, Sex Pistols, and the Misfits also influenced Metallica's
+				style as did post-punk band Killing Joke and hardcore punk acts Discharge and
+				Suicidal Tendencies. The band's early releases contained fast tempos, harmonized
+				leads, and nine-minute instrumental tracks. Steve Huey of AllMusic said Ride the
+				Lightning featured "extended, progressive epics; tight, concise groove-rockers".
+				Huey said Metallica expanded its compositional technique and range of expression
+				to take on a more aggressive approach in following releases, and lyrics dealt with
+				personal and socially conscious issues. Religious and military leaders, rage,
+				insanity, monsters, and drugs—among other themes—were explored on Master of
+			Puppets .</p>
+			<p>In 1991, Huey said Metallica with new producer Bob Rock simplified and streamlined
+				its music for a more commercial approach to appeal to mainstream audiences. Robert
+				Palmer of Rolling Stone said the band abandoned its aggressive, fast tempos to
+				expand its music and expressive range. The change in direction proved commercially
+				successful; Metallica was the band's first album to peak at number one on the
+				Billboard 200. Metallica noticed changes to the rock scene created by the grunge
+				movement of the early 1990s. In Load—an album that has been described as having
+				"an almost alternative rock" approach—the band changed musical direction and
+				focused on non-metal influences. Metallica's new lyrical approach moved away from
+				drugs and monsters, and focused on anger, loss, and retribution. Some fans and
+				critics were not pleased with this change, which included haircuts, the cover art
+				of Load, and headlining the Lollapalooza festival of 1996. David Fricke of
+				Rolling Stone described the move as "goodbye to the moldy stricture and dead-end
+				Puritanism of no-frills thrash", and called Load the heaviest record of 1996. With
+				the release of ReLoad in 1997, the band displayed blues and early hard rock
+			influences, incorporating more rhythm and harmony in song structures.</p>
+			<p>St. Anger marked another large change in the band's sound. Guitar solos were
+				excluded from the album, leaving a "raw and unpolished sound". The band used drop
+				C tuning; Ulrich's snare drum received particular criticism. New York Magazine's
+				Ethan Brown said it "reverberates with a thwong". The album's lyrics deal with
+				Hetfield's drug rehabilitation and include references to the devil, anti-drug
+				themes, claustrophobia, impending doom, and religious hypocrisy. At the advice of
+				producer Rick Rubin, for its ninth studio album Death Magnetic, the band returned
+				to standard tuning and guitar solos. As a return to Metallica's thrash roots,
+				Death Magnetic was a riff-oriented album featuring intense guitar solos and subtle
+			lyrics dealing with suicide and redemption.</p>
+			<h2>Legacy and influence</h2>
+			<figure>
+				<img src="images/stage.jpg" alt="Metallica on stage">
+				<figcaption>Metallica on stage</figcaption>
+			</figure>
+			<p>Metallica has become one of the most influential heavy metal bands of all time,
+				and is credited as one of the "big four" of thrash metal, along with Slayer,
+				Anthrax, and Megadeth. The band has sold more than 125 million records worldwide,
+				including an RIAA-certified 66 million and Nielsen SoundScan-reported 58,000,000
+				in the US, making Metallica one of the most commercially successful bands of all
+				time. The writers of The Rolling Stone Encyclopedia of Rock & Roll said Metallica
+				gave heavy metal "a much-needed charge". Stephen Thomas Erlewine and Greg Prato
+				of Allmusic said Metallica "expanded the limits of thrash, using speed and
+				volume not for their own sake, but to enhance their intricately structured
+				compositions", and called the band "easily the best, most influential heavy metal
+			band of the '80s, responsible for bringing the music back to Earth".</p>
+			<p>Jonathan Davis of Korn said he respects Metallica as his favorite band; he said,
+				"I love that they've done things their own way and they've persevered over the
+				years and they're still relevant to this day. I think they're one of the greatest
+				bands ever." Godsmack drummer Shannon Larkin said Metallica has been the biggest
+				influence on the band, stating, "they really changed my life when I was 16 years
+				old—I'd never heard anything that heavy". Vocalist and guitarist Robb Flynn of
+				Machine Head said that when creating the band's 2007 album, The Blackening,
+				"What we mean is an album that has the power, influence and epic grandeur of that
+				album Master of Puppets—and the staying power—a timeless record like that".
+				Trivium guitarists Corey Beaulieu and Matt Heafy said that when they heard
+				Metallica they wanted to start playing guitar. M. Shadows of Avenged Sevenfold
+				said touring with Metallica was the band's career highlight, and said,
+				"Selling tons of records and playing huge shows will never compare to meeting
+				your idols Metallica". God Forbid guitarists Doc and Dallas Coyle were inspired
+				by Metallica as they grew up, and the band's bassist John Outcalt admires Burton
+				as a "rocker". Ill Niño drummer Dave Chavarri said he finds early Metallica
+				releases are "heavy, raw, rebellious. It said, 'fuck you'", and Adema drummer
+			Kris Kohls said the band is influenced by Metallica.</p>
+			<p>On April 4, 2009, Metallica were inducted into the Rock And Roll Hall Of Fame.
+				They entered the Rock and Roll Hall of Fame the second year they were eligible
+				and first year they were nominated. Metallica's induction into the Hall included
+				its current lineup, James Hetfield, Kirk Hammett, Robert Trujillo, and Lars
+			Ulrich, and former members Jason Newsted and Cliff Burton.</p>
+		</article>
+		<aside>
+			<h2>Albums and awards</h2>
+			<p>Below are the studio albums, awards, and nominations Metallica recorded and
+				received throughout their career. The studio albums are listed in chronological
+			order from top to bottom.</p>
+			<h3>Studio Albums</h3>
+			<ul>
+				<li>Kill 'Em All</li>
+				<li>Ride the Lightning</li>
+				<li>Master of Puppets</li>
+				<li>...And Justice for All</li>
+				<li>Metallica</li>
+				<li>Load</li>
+				<li>Reload</li>
+				<li>St. Anger</li>
+				<li>Death Magnetic</li>
+				<li>Hardwired... to Self-Destruct</li>
+			</ul>
+			<h3>Awards and nominations</h3>
+			<ul>
+				<li>1990: Best Metal Performance – "One"</li>
+				<li>1991: Best Metal Performance – "Stone Cold Crazy"</li>
+				<li>1992: Best Metal Performance – Metallica</li>
+				<li>1999: Best Metal Performance – "Better than You"</li>
+				<li>2000: Best Hard Rock Performance – "Whiskey in the Jar"</li>
+				<li>2001: Best Rock Instrumental Performance – "The Call of Ktulu"</li>
+				<li>2004: Best Metal Performance – "St. Anger"</li>
+				<li>2009: Best Metal Performance – "My Apocalypse"</li>
+				<li>2009: Best Recording Package – Death Magnetic</li>
+			</ul>
+		</aside>
+		<footer>
+			<h2>Citations</h2>
+			<ul>
+				<li>Pereira, Alyssa (August 12, 2016).<a href="https://www.sfgate.com/music/article/Metallica-s-Black-Album-turns-25-9139432.php" target="_blank"> "Metallica's Black Album turns 25: Here's how local record stores reacted to its sales in 1991"</a>. San Francisco Chronicle. Retrieved April 20, 2018.</li>
+				<li><a href="https://www.metallica.com/band/history" target="_blank">"Band History"</a>. Metallica official website. Retrieved April 20, 2018.</li>
+				<li>Raguraman, Anjali (October 31, 2016).<a href="http://www.straitstimes.com/lifestyle/entertainment/metallica-shakes-it-up" target="_blank">"Metallica shakes it up"</a>. The Straits Times. Retrieved May 12, 2017.</li>
+				<li>Savage, Mark (February 14, 2018).<a href="https://www.bbc.com/news/entertainment-arts-43049190" target="_blank">"Metallica to get 'Nobel Prize of music'"</a>. BBC. Retrieved March 23, 2018.</li>
+			</ul>
+			<p>From Wikipedia: <a href="https://en.wikipedia.org/wiki/Metallica" target="_blank">Metallica</a>. This webpage, by Evan Pollack</p>
+		</footer>
+	</body>
+</html>
---
+++ jstart.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+	<head> 
+		<meta charset="utf-8">
+		<title>Lab 04 – Heraldry</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body>
+
+		<header>
+			<h1>Heraldry</h1>
+			<div>Coats of Arms and What They Mean</div>
+			<?php include "inc/nav.php"; ?>
+		</header>
+
+		<article>
+			<h2>Introduction</h2>
+			<figure>
+				<img src="images/herald.jpg" alt="A man in heraldic clothing holds a pointer, as if teaching.">
+				<figcaption>Thomas Hawley, Clarenceux King of Arms</figcaption>
+			</figure>
+			<p><strong>Heraldry</strong> (/ˈhɛrəldri/) is a broad term, encompassing the design, display, and study of armorial bearings (known as <strong>armory</strong>), as well as related disciplines, such as vexillology, together with the study of ceremony, rank, and pedigree. Armory, the best-known branch of heraldry, concerns the design and transmission of the heraldic achievement. The achievement, or armorial bearings usually includes a coat of arms on a shield, helmet, and crest, together with any accompanying devices, such as supporters, badges, heraldic banners, and mottoes.</p>
+			<p>Although the use of various devices to signify individuals and groups goes back to antiquity, both the form and use of such devices varied widely, and the concept of regular, hereditary designs, constituting the distinguishing feature of heraldry, did not develop until the High Middle Ages. It is very often claimed that the use of helmets with face guards during this period made it difficult to recognize one's commanders in the field when large armies gathered together for extended periods, necessitating the development of heraldry as a symbolic language, but there is very little actual support for this view.</p>
+			<p>The beauty and pageantry of heraldic designs allowed them to survive the gradual abandonment of armour on the battlefield during the seventeenth century. Heraldry has been described poetically as "the handmaid of history", "the shorthand of history", and "the floral border in the garden of history". In modern times, individuals, public and private organizations, corporations, cities, towns, and regions use heraldry and its conventions to symbolize their heritage, achievements, and aspirations.</p>
+			
+			<h2>Elements of a Heraldic Achievement</h2>
+			<figure>
+				<img src="images/shield.png" alt="A blank coat of arms, with all elements of an achievement present.">
+				<figcaption>Diagram of a heraldic achievement</figcaption>
+			</figure>
+			<p>A heraldic <strong>achievement</strong> consists of a shield of arms the <strong>coat of arms</strong>, or simply coat, together with all of its accompanying elements, such as a crest, supporters, and other heraldic embellishments. The term "coat of arms" technically refers to the shield of arms itself, but the phrase is commonly used to refer to the entire achievement. The one indispensable element of a coat of arms is the shield; many ancient coats of arms consist of nothing else, but no achievement or armorial bearings exists without a coat of arms.</p>
+			<p>From a very early date, illustrations of arms were frequently embellished with helmets placed above the shields. These in turn came to be decorated with fan-shaped or sculptural crests, often incorporating elements from the shield of arms; as well as a wreath or torse, or sometimes a coronet, from which depended the lambrequin or mantling. To these elements, modern heraldry often adds a <strong>motto</strong> displayed on a ribbon, typically below the shield.</p>
+			<p>If the bearer is entitled to the ribbon, collar, or badge of a knightly order, it may encircle or depend from the shield. Some arms, particularly those of the nobility, are further embellished with <strong>supporters</strong>, heraldic figures standing alongside or behind the shield; often these stand on a <strong>compartment</strong>, typically a mound of earth and grass, on which other badges, symbols, or heraldic banners may be displayed.</p>
+			
+			<h2>Shield (Escutcheon)</h2>
+			<p>The primary element of an heraldic achievement is the shield, or <strong>escutcheon</strong>, upon which the coat of arms is depicted. All of the other elements of an achievement are designed to decorate and complement these arms, but only the shield of arms is required. The shape of the shield, like many other details, is normally left to the discretion of the heraldic artist, and many different shapes have prevailed during different periods of heraldic design, and in different parts of Europe.</p>
+			<p>One shape alone is normally reserved for a specific purpose: the <strong>lozenge</strong>, a diamond-shaped escutcheon, was traditionally used to display the arms of women, on the grounds that shields, as implements of war, were inappropriate for this purpose. This distinction was not always strictly adhered to, and a general exception was usually made for sovereigns, whose arms represented an entire nation. Sometimes an oval shield, or <strong>cartouche</strong>, was substituted for the lozenge; this shape was also widely used for the arms of clerics in French, Spanish, and Italian heraldry, although it was never reserved for their use.</p>
+			<p>The whole surface of the escutcheon is termed the <strong>field</strong>, which may be plain, consisting of a single tincture, or divided into multiple sections of differing tinctures by various lines of partition; and any part of the field may be semé, or powdered with small charges. The edges and adjacent parts of the escutcheon are used to identify the placement of various heraldic charges; the upper edge, and the corresponding upper third of the shield, are referred to as the <strong>chief</strong>; the lower part is the <strong>base</strong>. The sides of the shield are known as the <strong>dexter</strong> and <strong>sinister</strong> flanks, although it is important to note that these terms are based on the point of view of the bearer of the shield, who would be standing behind it; accordingly the side which is to the bearer's right is the dexter, and the side to the bearer's left is the sinister, although to the observer, and in all heraldic illustration, the dexter is on the left side, and the sinister on the right.</p>
+			
+		</article>
+
+		<aside>
+			<h2>Tinctures</h2>
+			<p><strong>Tincture</strong> is the limited palette of colours and patterns used in heraldry. The need to define, depict, and correctly blazon the various tinctures is one of the most important aspects of heraldic art and design. The colours and patterns of the heraldic palette are divided into three groups, usually known as metals, colours, and furs. Two more tinctures were eventually acknowledged by most heraldic authorities. These were termed "stains" by some of the more influential heraldic writers.</p>
+			
+			<h3>Metals & Furs</h3>
+			<ul>
+				<li>Or</li>
+				<li>Argent</li>
+				<li>Ermine</li>
+				<li>Vair</li>
+				<li>Potent</li>
+				<li>Plumeté</li>
+				<li>Papelonné</li>
+				<li>Kürsch</li>
+			</ul>
+			
+			<h3>Colours & Stains</h3>
+			<ul>
+				<li>Gules</li>
+				<li>Sable</li>
+				<li>Azure</li>
+				<li>Vert</li>
+				<li>Purpure</li>
+				<li>Sanguine</li>
+				<li>Murrey</li>
+				<li>Tenné</li>
+			</ul>
+		</aside>
+
+		<footer>
+			<h2>Citations</h2>
+			<ul>
+				<li><a href="https://archive.org/details/completeguidetoh00foxdrich" target="_blank">Fox-Davies (1909), p. 1</a></li>
+				<li><a href="https://archive.org/details/heraldryancientm00boutrich" target="_blank">Boutell (1890), p. 6</a></li>
+				<li><a href="https://archive.org/details/treatiseonherald00wooduoft" target="_blank">Woodward & Burnett (1892), pp. 54–58</a></li>
+			</ul>
+			
+			<h2>Source</h2>
+			<ul>
+				<li><a href="https://en.wikipedia.org/wiki/Heraldry" target="_blank">Wikipedia Heraldry</a></li>
+				<li><a href="https://en.wikipedia.org/wiki/Tincture_(heraldry)" target="_blank">Wikipedia Tincture (heraldry)</a></li>
+				<li><a href="https://en.wikipedia.org/wiki/File:Thomas_Hawley_Clarenceux_King_of_Arms.jpg" target="_blank">Wikipedia File:Thomas Hawley Clarenceux King of Arms.jpg</a></li>
+				<li><a href="https://en.wikipedia.org/wiki/File:Elementy_herbu_mod.jpg" target="_blank">Wikipedia File:Elementy herbu mod.jpg</a></li>				
+			</ul>
+		</footer>
+
+	</body>
+</html>
